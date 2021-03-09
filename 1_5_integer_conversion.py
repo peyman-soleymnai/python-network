4262564b57a085bd3c7841eb6347cbd2e3d969ac
peyman-soleymani <soleymanip291@gmail.com>
2021-03-09 16:00:09 +0330

    this code is success run

---
+++ 1_5_integer_conversion.py
@@ -0,0 +1,15 @@
+#!/bin/python   
+
+
+import socket
+ 
+def convert_integer():
+    data = 1234
+    # 32-bit
+    print ("original: %s ==> Long host byte order: %s, Network byte order: %s" %(data, socket)
+    # 16-bit 
+    print ("original: %s ==> Short host byte order: %s, Network byte order: %s" %(data, socket)
+
+if __name__ == '__main__':
+
+    convert_integer()
